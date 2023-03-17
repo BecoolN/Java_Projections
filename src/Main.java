3b87564d3814619a81002537d048b3185564de6f
Kote <kote.papiashvili01@gmail.com>
2023-03-17 16:40:56 +0400

    Exceptions

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("input your age, please!");
-        Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
-        System.out.println(input);
+        try{
+            int number = Integer.parseInt("12");
+            System.out.println(number);
 
-        if(input <=15 ) {
-            System.out.println("you are a child!");
-        } else {
-            System.out.println("you are not child!");
+            for(int i = 10; i  >=0; i--){
+                System.out.println(10 / i);
+            }
+        } catch (NumberFormatException e){
+            System.out.println("failed parse X12 :");
+            System.out.println(e);
+        } catch (ArithmeticException a){
+            System.out.println(a);
         }
     }
 }
