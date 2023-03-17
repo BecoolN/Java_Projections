46b0ba44cca0922fca4ede1b92d5f13f4c3c285d
Kote <kote.papiashvili01@gmail.com>
2023-03-17 15:41:10 +0400

    BigDecimal

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,15 @@
-import java.time.*;
+import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("hi");
-        String[] number  ={
-                "kote",
-                "vaja",
-                "alexi",
-        };
-        String str= String.format("Number %s", number);
-        String str1 = String.join("/ " , number);
+        double a = 0.3;
+        double b = 0.1;
+        System.out.println(a - b);
 
-        System.out.println(str);
-        System.out.printf(str1);
-        System.out.println();
-        LocalDateTime localDateTime = LocalDateTime.now();
-        System.out.printf(String.valueOf(localDateTime));
-        System.out.println();
-        LocalDate localDate = LocalDate.now();
-        System.out.println(localDate.getMonth());
-        LocalDateTime localDate1 =localDateTime.of(
-                2000,
-                Month.DECEMBER,
-                23,
-                15,
-                33,
-                22
-
-        );
-        System.out.println(localDate1);
-
-        for (String zone : ZoneId.getAvailableZoneIds()) {
-            System.out.println(zone);
-        }
-
-        System.out.println(Instant.now());
+        BigDecimal ab = new BigDecimal("0.3");
+        BigDecimal ac = new BigDecimal("0.1");
+        BigDecimal result = ab.subtract(ac);
+        System.out.println(result);
 
     }
-
 }
